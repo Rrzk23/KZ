87937732ab60b05791db6eb9c99c749e439ffe92
wilson <wilsonzhu2003@gmail.com>
2024-09-14 22:39:40 +1000

    setting default value for .env

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -2,14 +2,15 @@ import { cleanEnv } from 'envalid';
 import { port, str } from 'envalid/dist/validators.js';
 
 const validateEnv = () => {
+  
 
   return cleanEnv(process.env, {
-    DB_URL: str(),
-    PORT: port(),
-    RAPIDAPI_KEY: str(),
-    SESSION_SECRET: str(),
+    DB_URL: str({ default: 'mongodb+srv://mock:QNsZWok9HjBTYdly@cluster0.rxrjtdf.mongodb.net/cool_price_app?retryWrites=true&w=majority&appName=Cluster0' }),
+    PORT: port({default: 5001}),
+    //RAPIDAPI_KEY: str(),
+    SESSION_SECRET: str({default: 'mock_session_secret'}),
     NODE_ENV: str({choices: ['development', 'production','test','CI']}),
-    REDIS_URL: str()
+    //REDIS_URL: str()
   });
 };
 
