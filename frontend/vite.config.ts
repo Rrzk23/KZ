e2ae7949eaf0f3ba2e02a508c7c990ae333f1c37
wilson <wilsonzhu2003@gmail.com>
2024-12-16 04:58:11 +1100

    adding backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
+    proxy: mode === 'development' ? {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       }
-    }
+    } : undefined,
   },
-})
+}));
+
